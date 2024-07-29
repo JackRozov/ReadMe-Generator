376937f1ee276aca16d60fac81fc81b91d86abac
jack <jackrozovsky@gmail.com>
2024-07-28 22:21:18 -0400

    Commit

--- index.js
+++ index.js
@@ -2,17 +2,16 @@
 var inquirer = require("inquirer");
 const generate = require("./assets/generate.js");
 const fs = require("fs");
-let fileName = "README-generated.md"
+let fileName = "Generated-ReadMe.md"
 
 // array of questions for user input
 const questions = {
-  title: "Project Title",
-  description: "Description of Project",
+  title: "Title",
+  description: "Description ",
   installation: "Installation Instructions",
   usage: "Usage Information",
-  contribute: "Contribution Guidelines",
+  contribute: "Contribution Guide",
   test: "Test Instructions",
-  license: "License",
   github: "Github Username",
   email: "Email Address",
   repo: "Please copy paste the repo URL",
@@ -22,9 +21,7 @@ const questions = {
 //function to write README file
 function writeToFile(response) {
   const title = `<h1 style= "text-center"> ${response.title} </h1>\n`;
-  //creates the file with just the title
   fs.writeFile(fileName, title, (error) => {
-    //or log an error if not
     if (error) {
       console.log(error);
     } else {
@@ -33,7 +30,6 @@ function writeToFile(response) {
   });
   //append the file with the rest of the markdown
   fs.appendFile(fileName, generate.generateMarkdown(response), (err) =>
-    //if theres an error log that, otherwise let user know that it work.
     err ? console.error(err) : console.log("information logged! Please check folder for your new README")
   );
 }
@@ -42,7 +38,6 @@ function writeToFile(response) {
 function init() {
   inquirer
     .prompt([
-      //all the questions are below
       {
         type: "input",
         message: questions.title,
@@ -73,27 +68,8 @@ function init() {
         message: questions.test,
         name: "test",
       },
-      {
-        type: "list",
-        message: questions.license,
-        name: "license",
-        choices: [
-          "none",
-          "Apache License 2.0",
-          "GNU General Public License v3.0",
-          "MIT License",
-          "BSD 2-Clause 'Simplified' License",
-          "BSD 3-Clause 'New' or 'Revised' License",
-          "Boost Software License 1.0",
-          "Creative Commons Zero v1.0 Universal",
-          "Eclipse Public License 2.0",
-          "GNU Affero General Public License v3.0",
-          "GNU General Public License v2.0",
-          "GNU Lesser General Public License v2.1",
-          "Mozilla Public License 2.0",
-          "The Unlicense",
-        ],
-      },
+     
+     
       {
           type: "input",
           message: questions.github,
@@ -119,15 +95,7 @@ function init() {
       console.log(response);
       writeToFile(response);
     })
-    .catch((error) => {
-      if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-        console.log(error);
-      } else {
-        // Something else went wrong
-        console.log(error);
-      }
-    });
+   
 }
 
 // Function call to initialize app
